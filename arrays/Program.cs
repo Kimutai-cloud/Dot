9983b0ba622c17e331df8b40647f8fa4b4522498
Kevin Korir <kevin.kimu0306@gmail.com>
2024-01-31 17:32:35 +0300

    arrays2

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -1,17 +1,39 @@
-﻿int startingInventory = 100;
-int[] inventory = new int[100];
+﻿int[,,] numbers = { { { 1, 4, 2, 4, 6 }, { 3, 6, 8, 9, 2 }, { 4, 7, 8,3,1 }, {5, 9, 1,5, 6 },{2, 7, 4, 7,8 } } };
+int ysum = 0, ysum1 = 0, ysum2 = 0, ysum3 = 0, ysum4 = 0;
+int yavg = 0, yavg1 = 0, yavg2 = 0; yavg3 = 0; yavg4 = 0;
 
-
-for (int i = 0; i < inventory.Length; i++)
+for (int i = 0; i < numbers.GetLength(0); i++)
 {
-    inventory[i] = startingInventory;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        for (int k = 0; k < numbers.GetLength(2); k++)
+        {
+            if (j == 0)
+            {
+                ysum += numbers[i, j, k];
+            }
+            if (j == 1)
+            {
+                ysum1 += numbers[i, j, k];
+            }
+            if (j == 2)
+            {
+                ysum2 += numbers[i, j, k];
+            }
+            if (j == 3)
+            {
+                ysum3 += numbers[i, j, k];
+            }
+            if (j == 4)
+            {
+                ysum4 += numbers[i, j, k];
+            }
+        }
+    }
 }
 
-int bin = 0;
-
-foreach (int item in inventory)
-{
-    bin++;
-    Console.WriteLine($"Bin {bin} = {item} Inventory: Running total {item * bin}");
-}
-Console.WriteLine($"We have {inventory.Sum()} items in inventory.");
+Console.WriteLine($"Sum along y-axis for first element: {ysum}");
+Console.WriteLine($"Sum along y-axis for second element: {ysum1}");
+Console.WriteLine($"Sum along y-axis for third element: {ysum2}");
+Console.WriteLine($"Sum along y-axis for fourth element: {ysum3}");
+Console.WriteLine($"Sum along y-axis for fifth element: {ysum4}");

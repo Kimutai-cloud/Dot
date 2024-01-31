60934937f0fc1b194d232f89feb7047a425d3cdc
Kevin Korir <kevin.kimu0306@gmail.com>
2024-01-31 09:29:18 +0300

    arrays

---
+++ arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿int startingInventory = 100;
+int[] inventory = new int[100];
+
+
+for (int i = 0; i < inventory.Length; i++)
+{
+    inventory[i] = startingInventory;
+}
+
+int bin = 0;
+
+foreach (int item in inventory)
+{
+    bin++;
+    Console.WriteLine($"Bin {bin} = {item} Inventory: Running total {item * bin}");
+}
+Console.WriteLine($"We have {inventory.Sum()} items in inventory.");

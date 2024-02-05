61d15f5c83e36011359c7714612149b623903144
Kevin Korir <kevin.kimu0306@gmail.com>
2024-02-05 16:56:41 +0300

    bulk flips

--- Bools/Program.cs
+++ Bools/Program.cs
@@ -1,29 +1,66 @@
 ﻿Random random  = new Random();
 int flip = random.Next(2);
+int flips;
+int numberedFlips;
 
 Console.WriteLine("\t\t!!Coin Flip Game!!\nPlease enter your name:");
 string playerName = Console.ReadLine();
 string playerChoice;
-int flips;
-Console.WriteLine($"Welcome {playerName}");
-do
+
+
+Console.WriteLine($"Welcome {playerName}, do you want to play the normal flip(normal) or bulk flip(bulk)?");
+string gameChoice = Console.ReadLine().Trim().ToLower();
+
+if (gameChoice == "normal")
 {
-    
-    Console.WriteLine("If you want to flip a coin enter (Flip) if not enter (Exit).");
-    playerChoice = Console.ReadLine().Trim().ToLower();
-    if (playerChoice == "flip")
+    do
     {
-        flips = random.Next(2);
-        if (flips == 0)
+        Console.WriteLine("If you want to flip a coin enter (Flip) if not enter (Exit).");
+        playerChoice = Console.ReadLine().Trim().ToLower();
+        if (playerChoice == "flip")
         {
-            Console.WriteLine("Heads");
+            flips = random.Next(2);
+            if (flips == 0)
+            {
+                Console.WriteLine("Heads");
+            }
+            else if (flips == 1)
+            {
+                Console.WriteLine("Tails");
+            }
         }
-        else if(flips == 1)
+    } while (playerChoice == "flip");
+    Console.WriteLine("Thank you and see you soon!!");
+}
+else
+{
+    Console.WriteLine("How many flips do you want?.");
+    numberedFlips = int.Parse(Console.ReadLine().Trim());
+    do
+    {
+        
+
+        Console.WriteLine("If you are ready to bulk flip a coin enter (Flip) if not enter (Exit).");
+        playerChoice = Console.ReadLine().Trim().ToLower();
+        for(int i = 0; i <= numberedFlips; i++)
         {
-            Console.WriteLine("Tails");
+            if (playerChoice == "flip")
+            {
+                flips = random.Next(2);
+                if (flips == 0)
+                {
+                    Console.WriteLine("Heads");
+                }
+                else if (flips == 1)
+                {
+                    Console.WriteLine("Tails");
+                }
+            }
         }
-    }
-} while (playerChoice == "flip");
-Console.WriteLine("Thank you and see you soon!!");
+        
+    } while (playerChoice == "flip");
+    Console.WriteLine("Thank you and see you soon!!");
+}
+
 
  

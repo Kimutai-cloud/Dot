7f7d8ed4ce1508fc641c9b69b21bb1a765ccca26
Kevin Korir <kevin.kimu0306@gmail.com>
2024-02-06 15:28:18 +0300

    added replay function and a bit on player leveling

--- dorpg/Program.cs
+++ dorpg/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Welcome to Alverdale, please enter you player name:");
+﻿Console.WriteLine("Welcome to Alverdale, please enter you Player name:");
 string playerName = Console.ReadLine();
 
 
@@ -7,34 +7,49 @@ string playerName = Console.ReadLine();
 
 
 Random rand = new Random();
-int playerAttackPoints, monsterAttackPoints;
-int playerHP = 10, monsterHP = 10;
+string choice;
+int playerAttackPoints, monsterAttackPoints, lvlCaps;
+int playerHP = 10, monsterHP = 10, playerLvl = 0, monsterLvl = 0, playerExp = 0;
+
 do
 {
-    playerAttackPoints = rand.Next(1, 11);
-    
-    monsterHP = monsterHP - playerAttackPoints;
-    if (monsterHP < 0)
+    do
     {
-        monsterHP = 0;
-    }
-    Console.WriteLine($"PLayer {playerName} attacked, Monster was damaged and lost {playerAttackPoints} health and now has {monsterHP} health.");
-    if( monsterHP <= 0)
-    {
-        Console.WriteLine("The Monster has been slain!!");
-        break;
-    }
+        playerAttackPoints = rand.Next(1, 11);
 
-    monsterAttackPoints = rand.Next(0, 10);
-    playerHP = playerHP - monsterAttackPoints;
-    if(playerHP < 0)
-    {
-        playerHP = 0;
-    }
-    Console.WriteLine($"The Monster attacked, Player {playerName} was damaged and lost {monsterAttackPoints} health and now has {playerHP} health.");
-    if ( playerHP <= 0)
-    {
-        Console.WriteLine($"Player {playerName} have been defeated");
-        break;
-    }
-}while( monsterHP > 0 && playerHP > 0 );
+        monsterHP = monsterHP - playerAttackPoints;
+        if (monsterHP < 0)
+        {
+            monsterHP = 0;
+        }
+        Console.WriteLine($"PLayer {playerName} attacked, Monster was damaged and lost {playerAttackPoints} health and now has {monsterHP} health.");
+        if (monsterHP <= 0)
+        {
+            Console.WriteLine("The Monster has been slain!!");
+            playerExp += 10;
+            if (playerExp >= 20)
+            {
+                playerLvl = 1;
+                Console.WriteLine($"Congratulations Player {playerName}, you have leveled up to level {playerLvl}!");
+            }
+            break;
+        }
+
+        monsterAttackPoints = rand.Next(0, 10);
+        playerHP = playerHP - monsterAttackPoints;
+        if (playerHP < 0)
+        {
+            playerHP = 0;
+        }
+        Console.WriteLine($"The Monster attacked, Player {playerName} was damaged and lost {monsterAttackPoints} health and now has {playerHP} health.");
+        if (playerHP <= 0)
+        {
+            Console.WriteLine($"Player {playerName} have been defeated");
+            break;
+        }
+    } while (monsterHP > 0 && playerHP > 0);
+    monsterHP = 10;
+    playerHP = 10;
+    Console.WriteLine("Do you want to play again(play) on proceed(proceed)?");
+    choice = Console.ReadLine().Trim().ToLower();
+} while (choice == "play");

e01eeb79c4e6494afb64d03ad9b79df721f93ddc
Kevin Korir <kevin.kimu0306@gmail.com>
2024-02-06 09:54:34 +0300

    fizzbuzz challenge

--- switch/Program.cs
+++ switch/Program.cs
@@ -1,55 +1,23 @@
-﻿// SKU = Stock Keeping Unit
-string sku = "01-MN-L";
-
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-switch (product[0])
+﻿for (int i = 1; i <= 100; i++)
 {
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
-    default:
-        type = "Other";
-        break;
+    bool isDivisibleBy3 = i % 3 == 0;
+    bool isDivisibleBy5 = i % 5 == 0;
+
+    if (isDivisibleBy3 && isDivisibleBy5)
+    {
+        Console.WriteLine(i + ": FizzBuzz");
+    }
+    else if (isDivisibleBy3)
+    {
+        Console.WriteLine(i + ": Fizz");
+    }
+    else if (isDivisibleBy5)
+    {
+        Console.WriteLine(i + ": Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
 }
 
-switch (product[1])
-{
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;
-}
-
-switch (product[2])
-{
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
